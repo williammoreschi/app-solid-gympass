25552d1c4f6de9fd2e9f23054b408fb6d491de95
william <william.moreschi@guiadomarceneiro.net>
2023-06-20 17:48:29 -0300

    Configurando ESLint

--- env/index.ts
+++ env/index.ts
@@ -1,5 +1,5 @@
-import {config} from 'dotenv';
-import { z } from 'zod';
+import { config } from 'dotenv'
+import { z } from 'zod'
 
 if (process.env.NODE_ENV === 'test') {
   config({ path: '.env.test' })
