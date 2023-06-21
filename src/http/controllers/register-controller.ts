818927f6d77fb9fc749ec1db74b906da12d58078
william <william.moreschi@guiadomarceneiro.net>
2023-06-21 15:30:22 -0300

    separando as rotas em outro arquivo, criando controller registro

---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+export const register = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
