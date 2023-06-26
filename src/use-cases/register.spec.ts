37d5210dee6d4a1e4e3d54431953e4a60678495c
william <william.moreschi@guiadomarceneiro.net>
2023-06-26 10:33:32 -0300

    Primeiro Teste Unitario. No register.ts foi necessario devolver o cliente pra conseguir fazer o teste

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,32 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash user upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+      async findByEmail(email) {
+        return null
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'Jonh Doe',
+      email: 'jonh.doe@example.com',
+      password: '123456',
+    })
+
+    const isPassworCorrectlyHashed = await compare('123456', user.password_hash)
+
+    expect(isPassworCorrectlyHashed).toBe(true)
+  })
 })
