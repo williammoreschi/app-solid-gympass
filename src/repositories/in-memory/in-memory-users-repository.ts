8b40a2d92da20e30dc8f3ff01c5c314ef63c404e
william <william.moreschi@guiadomarceneiro.net>
2023-06-28 14:58:06 -0300

    Criação Caso de uso de perfil

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,16 @@ import { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public itens: User[] = []
 
+  async findById(id: string) {
+    const user = this.itens.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = this.itens.find((item) => item.email === email)
 
