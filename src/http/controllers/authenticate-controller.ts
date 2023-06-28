01b22e1dd430b25a8459430f10622dcc914907f5
william <william.moreschi@guiadomarceneiro.net>
2023-06-28 13:42:23 -0300

    criado o ontroller que receberá a requisição de autenticação e adcionado a rota na api

---
+++ src/http/controllers/authenticate-controller.ts
@@ -0,0 +1,32 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prismas-users-repository'
+import { AuthenticateUserCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export const authenticate = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const userRespositry = new PrismaUsersRepository()
+    const authenticatedUseCase = new AuthenticateUserCase(userRespositry)
+
+    await authenticatedUseCase.execute({ email, password })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
+  }
+
+  return reply.status(200).send()
+}
