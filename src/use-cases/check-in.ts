cdfe9095d8287132345c2937c336960857064314
william <william.moreschi@guiadomarceneiro.net>
2023-06-28 15:42:47 -0300

    Criado o Caso de uso de check-in

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkeInRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkeInRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return { checkIn }
+  }
+}
