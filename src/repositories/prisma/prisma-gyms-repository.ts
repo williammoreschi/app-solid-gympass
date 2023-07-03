cc17d6c0b99f47c5217555a535a755cf6460b959
william <william.moreschi@guiadomarceneiro.net>
2023-07-03 18:59:14 -0300

    erro de escrita

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -2,7 +2,7 @@ import { Gym, Prisma } from '@prisma/client'
 import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { prisma } from '@/lib/prisma'
 
-export class PrismsGymsRepository implements GymsRepository {
+export class PrismaGymsRepository implements GymsRepository {
   async findById(id: string) {
     const gym = await prisma.gym.findUnique({
       where: { id },
