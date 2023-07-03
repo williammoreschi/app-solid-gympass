28c5a5e5a3ab36a2493c0e6babd8ee8e94bf7a61
william <william.moreschi@guiadomarceneiro.net>
2023-07-03 13:54:50 -0300

    implementação dos repositórios check-ins do Prisma

--- src/repositories/prisma/prismas-users-repository.ts
+++ src/repositories/prisma/prismas-users-repository.ts
@@ -4,7 +4,11 @@ import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
   async findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+    const user = await prisma.user.findUnique({
+      where: { id },
+    })
+
+    return user
   }
 
   async findByEmail(email: string) {
