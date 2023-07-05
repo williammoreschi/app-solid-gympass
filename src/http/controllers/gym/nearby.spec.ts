debe3a495494d18abcdb2ec3ffd61d696ce64ed9
william <william.moreschi@guiadomarceneiro.net>
2023-07-05 17:22:16 -0300

    Testes E2E de rotas de academias

---
+++ src/http/controllers/gym/nearby.spec.ts
@@ -0,0 +1,57 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Nearyby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near JavaScript Gym',
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far JavaScript Gym',
+        description: null,
+        phone: null,
+        latitude: -23.4811505,
+        longitude: -51.8056056,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near JavaScript Gym',
+      }),
+    ])
+  })
+})
---
+++ src/http/controllers/gym/register.spec.ts
@@ -0,0 +1,30 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
