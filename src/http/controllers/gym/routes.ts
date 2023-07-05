7b10ac0d686915745a8860bd1393894dd9e74f41
william <william.moreschi@guiadomarceneiro.net>
2023-07-05 13:29:07 -0300

    Criado o controller de buscar academias

--- src/http/controllers/gym/routes.ts
+++ src/http/controllers/gym/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { register } from './register-controller'
+import { search } from './search-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
 
   app.post('/gyms', register)
 }
---
+++ src/http/controllers/gym/search-controller.ts
@@ -0,0 +1,17 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export const search = async (request: FastifyRequest, reply: FastifyReply) => {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.query)
+
+  const registerUseCase = makeSearchGymsUseCase()
+  const { gyms } = await registerUseCase.execute({ query, page })
+
+  return reply.status(201).send({ gyms })
+}
