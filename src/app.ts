58d616aa2534e9bc423a44b39f8c596031914313
william <william.moreschi@guiadomarceneiro.net>
2023-07-08 08:46:05 -0300

    Implementado o refresh token

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
     cookieName: 'refreshToken',
-    signed: false,
+    signed: false, //  cookie é assinado?
   },
   sign: {
     expiresIn: '10m',
