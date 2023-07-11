b02b0cdb5c84fd43e5ccbb0db9c784e15d168218
william <william.moreschi@guiadomarceneiro.net>
2023-07-11 12:25:08 -0300

    corrigindo logica de criacao de usuario de teste

--- src/utils/tests/create-and-authenticate-user.ts
+++ src/utils/tests/create-and-authenticate-user.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
@@ -5,14 +7,14 @@ export async function createAndAuthenticateUser(
   app: FastifyInstance,
   isAdmin = false,
 ) {
-  await request(app.server)
-    .post('/users')
-    .send({
+  await prisma.user.create({
+    data: {
       name: 'John Doe',
       email: 'john.doe@example.com',
-      password: '123456',
+      password_hash: await hash('123456', 6),
       role: isAdmin ? 'ADMIN' : 'MEMBER',
-    })
+    },
+  })
 
   const Authenticateresponse = await request(app.server)
     .post('/sessions')
@@ -21,7 +23,7 @@ export async function createAndAuthenticateUser(
       password: '123456',
     })
 
-  const token = Authenticateresponse.body.token
+  const { token } = Authenticateresponse.body
 
   return { token }
 }
